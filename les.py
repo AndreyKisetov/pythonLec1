a32251c15f578ba6073359c63e12263a820f041b
Andrey Kisetov <rbcfff@yandex.ru>
2022-09-03 05:57:59 +0300

    added les.py

---
+++ les.py
@@ -0,0 +1,119 @@
+# print('hello world')
+
+# value = None
+a=123
+b=1.23
+# print(type(a))
+# print(type(b))
+# vaiue = 12334
+# print(type(vaiue))
+# s = "hello world"
+
+# print(s)
+# print(a,'-',b,'-',s)
+# print('{1} - {2} - {0}'.format(a,b,s))
+# print(f'{a} - {b} - {s}')
+
+# f = True
+# print(f)
+
+# list = ['1','2','3','hello']
+# col = ['hello', 1,2,4.5, True]
+# print(list)
+# print(col)
+
+# print('Введите а')
+# a = int(input())
+# print('Введите в')
+# b = int(input())
+# print(a, b, ' ', a+b)
+
+# a = 1.31234556
+# b = 3
+# c = round(a * b, 5)
+# print(c)
+
+# a = 3
+# a += 5
+# print(a)
+
+# a = 1 != 2
+# print(a)
+
+# func = 1
+# T = 4
+# x = 1
+# print(func <T> (x))
+
+# f = 1 > 2 or 4 < 6
+# print(f)
+
+# f = [1,2,3,4]
+# print(f)
+# print(2 in f)
+
+# is_odd = f[0] % 2 == 0
+# print(is_odd)
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+# username = input('Введите имя:')
+# if username == 'Маша':
+#     print('Ура, это же МАША!')
+# elif username == 'Миша':
+#     print('Я так ждала вас МИША!')
+# elif username == 'Мила':
+#     print('Ура, это же МИЛА!')
+# else:
+#     print('Привет, ', username)
+
+# original = 23
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+# else:
+#     print('Пожалуй')
+#     print('хватит )')
+# print(inverted)
+
+# for i in 1,2,3,4,5:
+#     print(i**2)
+
+# list = [1,2,3,4,10,5]
+# for i in list:
+#     print(i)
+
+# r = range(10)
+# for i in r:
+#     print(i)
+
+# for i in range(1, 10, 2):
+#     print(i)
+
+# for i in r"qwe - rty":
+#     print(i)
+
+# def f(x):
+#     if x == 1:
+#         return 'Целое'
+#     elif x == 2.3:
+#         return 23
+#     else:
+#         return
+
+# arg = 2
+# print(f(arg))
+# print(type(f(arg)))
+
+
+
+
+
+
+
